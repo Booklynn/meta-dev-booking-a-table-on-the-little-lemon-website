8c067abf2df507c49d118a4a9412989a04e7bab4
Booklynn <95514331+Booklynn@users.noreply.github.com>
2024-11-03 15:51:17 +0700

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders little lemon title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Little Lemon/i);
+  expect(titleElement).toBeInTheDocument();
 });
